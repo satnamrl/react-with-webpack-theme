191c7ec36c8942f575e32fdddf5d01d8b92cee46
satnam singh <satnam@solulab.com>
2023-01-24 12:06:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const sayHelloManyTimes = (times) =>
+  new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
+
+const helloDiv = document.createElement("div");
+helloDiv.innerHTML = sayHelloManyTimes(15).join("<br/>");
+document.body.append(helloDiv);
